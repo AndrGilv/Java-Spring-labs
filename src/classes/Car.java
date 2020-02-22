c88b5c00522a8213db99e114921797a77521a7b2
Andrey Gikev <andrgilv@gmail.com>
2020-02-22 18:16:28 +0700

    initial commit

---
+++ src/classes/Car.java
@@ -0,0 +1,26 @@
+package classes;
+
+public class Car extends Transport {
+
+    private String transmission;// задний, передний, полный привод
+
+    public Car(String name, String type, int numberOfSeats, String transmission) {
+        super("Автомобиль", name, type, numberOfSeats);
+        this.transmission = transmission;
+    }
+
+    public Car(String type) {
+        super("Автомобиль", type);
+
+    }
+
+    public String getTransmission() {
+
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+
+        this.transmission = transmission;
+    }
+}
