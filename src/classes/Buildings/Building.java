0b40be1913f38523166df32b6493c87a916d5c10
Andrey Gikev <andrgilv@gmail.com>
2020-04-25 13:59:42 +0700

    Add Linker and Iterator (Lab 9)

---
+++ src/classes/Buildings/Building.java
@@ -0,0 +1,61 @@
+package classes.Buildings;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Building extends BuildingItem {
+
+    ArrayList<BuildingItem> items = new ArrayList<BuildingItem>();
+
+    public Building( String name) {
+        super( name);
+    }
+
+    @Override
+    public BuildingItem add(BuildingItem building) {
+
+        items.add(building);
+        return this;
+    }
+
+    @Override
+    public BuildingItem remove(BuildingItem building) {
+
+        items.remove(building);
+        return this;
+    }
+
+    @Override
+    public BuildingItem getChild(int i) {
+        return items.get(i);
+    }
+
+    @Override
+    public Iterator getIterator() {
+        return items.iterator();
+    }
+
+    @Override
+    public int getSpace() {
+        int space = 0;
+        for(BuildingItem item : items){
+            space += item.getSpace();
+        }
+        return space;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("building ");
+        stringBuilder.append(name);
+        stringBuilder.append(", space ");
+        stringBuilder.append(getSpace());
+        stringBuilder.append(" m2\n");
+        for(BuildingItem item : items) {
+            stringBuilder.append("\t" + item.toString());
+        }
+        stringBuilder.append("\n");
+        return stringBuilder.toString();
+    }
+}
---
+++ src/classes/Buildings/Flat.java
@@ -0,0 +1,32 @@
+package classes.Buildings;
+
+public class Flat extends BuildingItem {
+
+    private int space;
+
+    public Flat(int space, String name) {
+        super( name);
+        this.space = space;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Flat ");
+        builder.append(name);
+        builder.append(", space ");
+        builder.append(space);
+        return builder.toString();
+    }
+
+    @Override
+    public void setSpace(int space) {
+        this.space = space;
+    }
+
+    @Override
+    public int getSpace() {
+        return space;
+    }
+
+}
